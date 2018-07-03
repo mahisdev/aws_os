294d9cd115901d027aa04b766c781664e2012ca7
mahisdev <mahendram1906@gmail.com>
2018-07-03 11:14:09 +0000

    new script

---
+++ formation.sv
@@ -0,0 +1,16 @@
+# This file is SVn file which
+# is having evn variable
+
+Cloude=azure
+Resource=ec2
+Name=Test
+enveronment=Test
+
+
+Cloude=azure
+Resource=ec2
+Name=Test
+enveronment=Test
+
+
+
